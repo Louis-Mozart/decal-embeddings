ad56ea1c24e5f47b22a307aeaab819bfc12c0c82
Louis Mozart KAMDEM TEYOU <93260304+Louis-Mozart@users.noreply.github.com>
2025-09-23 13:17:36 +0200

    Update GSDC_performance.py

--- GSDC_performance.py
+++ GSDC_performance.py
@@ -22,7 +22,7 @@ class show_performance:
         
                     
     def plot3d(self):
-        """This plot will show the distribution of all possible combinations in the GSDC search"""
+        """This plot will show the distribution of all possible combinations in the GSDC search. This will be made in a [0, d]^3"""
 
         fig = plt.figure(figsize=(8, 6))
         ax = fig.add_subplot(111, projection='3d')
